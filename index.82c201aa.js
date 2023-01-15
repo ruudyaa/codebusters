e2293cbb53ec28e3397a29da14a0c8b7fa73f671
blowmybow <115591313+blowmybow@users.noreply.github.com>
2023-01-15 21:29:04 +0000

    Deploying to gh-pages from @ ruudyaa/codebusters@13cb0a4ab9ae887d886980bcf78f3c38eef4083d 🚀

---
+++ index.82c201aa.js
@@ -0,0 +1,2 @@
+$(".back-to-top").click((function(){$("body,html").animate({scrollTop:0},800)})),$(window).scroll((function(){$(window).scrollTop()>250?$(".back-to-top").addClass("active"):$(".back-to-top").removeClass("active")}));
+//# sourceMappingURL=index.82c201aa.js.map
