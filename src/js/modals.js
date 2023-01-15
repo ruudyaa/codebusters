292f2ef019df5b960b693b2016e04b5c5e63a26e
NataliaNazarenko <115888204+NataliaNazarenko@users.noreply.github.com>
2023-01-15 16:31:09 +0200

    fixed bug

--- src/js/modals.js
+++ src/js/modals.js
@@ -1,3 +1,24 @@
+// $('.js-open-modal').click(function (event) {
+//   event.preventDefault();
+
+//   var modalName = $(this).attr('data-modal');
+//   var modal = $('.js-modal[data-modal="' + modalName + '"]');
+//   modal.addClass('is-hidden');
+//   $('.js-overlay-modal').addClass('is-hidden');
+// });
+
+// $('.js-modal-close').click(function () {
+//   $(this).parent('.js-modal').removeClass('is-hidden');
+//   $('.js-overlay-modal').removeClass('is-hidden');
+// });
+
+// $('.js-overlay-modal').click(function () {
+//   $('.js-modal.is-hidden').removeClass('is-hidden');
+//   $(this).removeClass('is-hidden');
+// });
+
+// var linkArray = document.querySelectorAll('.js-open-modal');
+
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
