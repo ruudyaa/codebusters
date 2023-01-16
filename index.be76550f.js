23f13a7ca02c8d14017ffb11cf7a01d06df2664d
ruudyaa <73854990+ruudyaa@users.noreply.github.com>
2023-01-16 00:16:48 +0000

    Deploying to gh-pages from @ ruudyaa/codebusters@d80a2492ad10f0580a0fce0fae220df095cd60cf 🚀

--- index.be76550f.js
+++
@@ -1,2 +0,0 @@
-$(".back-to-top").click((function(){$("body,html").animate({scrollTop:0},800)})),$(window).scroll((function(){$(window).scrollTop()>250?$(".back-to-top").addClass("active"):$(".back-to-top").removeClass("active")}));
-//# sourceMappingURL=index.be76550f.js.map
