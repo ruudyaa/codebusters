c2be73d7739f9e406b82479856c63204db20d742
ruudyaa <martarudnytska26@gmail.com>
2023-01-21 20:29:16 +0200

    Update scroll_off.js

--- src/js/scroll_off.js
+++ src/js/scroll_off.js
@@ -3,22 +3,21 @@ const btnOn = document.querySelector('.js-modal-scrollon');
 const body = document.body;
 
 function disableScroll() {
-    body.classList.add('disable-scroll');
+  body.classList.add('disable-scroll');
 }
 
 function enableScroll() {
-    body.classList.remove('disable-scroll');
+  body.classList.remove('disable-scroll');
 }
 
-btnOff.addEventListener('click', (e) => {
-    enableScroll()
-    e.currentTarget.style.pointerEvents = 'none';
-    btnOn.style.pointerEvents = 'auto'
-}) 
+btnOff.addEventListener('click', e => {
+  enableScroll();
+  e.currentTarget.style.pointerEvents = 'none';
+  btnOn.style.pointerEvents = 'auto';
+});
 
-btnOn.addEventListener('click', (e) => {
-    disableScroll()
-    e.currentTarget.style.pointerEvents = 'none';
-    btnOff.style.pointerEvents = 'auto'
-}) 
-    
+btnOn.addEventListener('click', e => {
+  disableScroll();
+  e.currentTarget.style.pointerEvents = 'none';
+  btnOff.style.pointerEvents = 'auto';
+});
